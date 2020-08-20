cadeae6c999dd82f30c1b5e58262d94f26246eaf
Pankaj Verma <pankaj065@gmail.com>
2020-08-20 16:51:39 +0530

    Update create-k8s-cluster.bat

--- create-k8s-cluster.bat
+++ create-k8s-cluster.bat
@@ -2,4 +2,4 @@
 
 vagrant up
 
-vagrant ssh -c "sudo cd /vagrant; ansible-playbook -vvv -i provisioning/inventory/hosts provisioning/playbook.yml" k8smaster
+vagrant ssh -c "cd /vagrant; ansible-playbook -vvv -i provisioning/inventory/hosts provisioning/playbook.yml" k8smaster
