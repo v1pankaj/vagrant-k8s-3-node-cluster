4699c4acb63b5b5bb3755fa9bb5612d766ab2e25
Pankaj Verma <pankaj065@gmail.com>
2020-08-20 13:55:37 +0530

    Update create-k8s-cluster.sh

--- create-k8s-cluster.sh
+++ create-k8s-cluster.sh
@@ -4,4 +4,4 @@
 vagrant up
 
 ### Logging into k8smaster vbox machine ###
-vagrant ssh -c "cd /vagrant; ansible-playbook -vvv -i provisioning/inventory/hosts provisioning/playbook.yml" k8smaster
+vagrant ssh -c "sudo cd /vagrant; ansible-playbook -vvv -i provisioning/inventory/hosts provisioning/playbook.yml" k8smaster
